3c45931914c6bdcd8262b9debc5bac0acbe75129
Rajarshi Roy <roystark.dev@gmail.com>
2025-10-05 21:08:00 +0530

    created array folder

---
+++ easy/array/1752_check_if_array_is_sorted_and_rotated.py
@@ -0,0 +1,24 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        count = 0
+
+        '''
+        logic: if there is more than one drop in value in adjacent elements then
+        the array was never sorted in the initial state. so we are just checking
+        the count of drop in values between consecutive elements.
+        '''
+        for i in range(0, len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                count += 1
+
+        '''
+        this is to handle the comparison between last and the first element
+        because the arrays are rotated.
+        '''
+        if nums[len(nums) - 1] > nums[0]:
+            count += 1
+
+        if count > 1:
+            return False
+        else:
+            return True
