0e63c60c7409206708fadb9449c2c18ff6519f87
Rajarshi Roy <roystark.dev@gmail.com>
2025-10-05 21:09:19 +0530

    solved 206

---
+++ 206_reverse_linked_list.py
@@ -0,0 +1,13 @@
+# Optimal solution
+# Time complexity O(N)
+# Space Complexity O(1)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        prev = None
+        while temp:
+            front = temp.next
+            temp.next = prev
+            prev = temp
+            temp = front
+        return prev
